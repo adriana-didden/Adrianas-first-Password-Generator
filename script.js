230c9d48b17b5d464233922430a704ef970e36fb
AJD <adriana.j.didden@gmail.com>
2020-01-30 15:47:54 -0500

    added comments in script.js that layout the steps that need to be completed.

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//onclick - present series of prompts
+        //prompt 1 select length of password 8-128 characters
+
+        //prompt 2 select if lowercase is allowed
+                //input validated and at least 1 character shown
+
+        //prompt 3 select if Uppercase is allowed
+                  //input validated and at least 1 character shown
+        
+        //prompt 4 select if numeric is allowed
+                  //input validated and at least 1 character shown
+
+
+        //prompt 5 select if special characters are allowed
+                //input validated and at least 1 character shown
+
+        //after ALL prompts are answered, password is generated
+        
+        //the password is then shown in an alert or written on the screen
+
