c63e4d18ea0be032e60a0f3952d8757e52bcc4b3
AJD <adriana.j.didden@gmail.com>
2020-01-31 14:09:15 -0500

    added arrays to script file

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Password-Generator
+write javascript code in order to allow the user to generate a password to their specifications. 
+using loops, arrays, prompts, functions, and alerts. 
--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+//MY CODE 
+var length = prompt("choose length of password between 7 and 128")
+var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var Uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = [! @ # $ % ^ & * ( ) _ + = - ?];
+var numeric = [0 1 2 3 4 5 6 7 8 9];
+
+
+function prompts() {
+  if
+}
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -13,15 +25,16 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
+
 //onclick - present series of prompts
         //prompt 1 select length of password 8-128 characters
-
         //prompt 2 select if lowercase is allowed
+
                 //input validated and at least 1 character shown
 
         //prompt 3 select if Uppercase is allowed
                   //input validated and at least 1 character shown
-        
+
         //prompt 4 select if numeric is allowed
                   //input validated and at least 1 character shown
 
@@ -30,6 +43,6 @@ generateBtn.addEventListener("click", writePassword);
                 //input validated and at least 1 character shown
 
         //after ALL prompts are answered, password is generated
-        
+
         //the password is then shown in an alert or written on the screen
 
