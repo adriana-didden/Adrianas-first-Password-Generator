0b47f85e4a08cc1ace450c4d50edead951c7ebb8
AJD <adriana.j.didden@gmail.com>
2020-02-02 01:31:07 -0500

    forgot to commit as i went so this is most of the work in my script file

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-//MY CODE 
-var length = prompt("choose length of password between 7 and 128")
-var lowercase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var Uppercase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "+", "=", "-", "?"];
-var numeric = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+//container of code that activates when button is clicked
+function generate() {
+        var length = Number(prompt("What length would you like your password to be?"));
 
+        while (isNaN(length) || length < 8 || length > 128) length = Number(prompt("try again! Length must be 8-128 characters."));
 
-function prompts() {
-  if
+        var UC = confirm("Would you like to use uppercase letters?");
+        var LC = confirm("Would you like to use lowercase letters?");
+        var num = confirm("Would you like to use numbers?");
+        var special = confirm("Would you like to use special characters?");
+
+        while (!UC && !LC && !num && !special) {
+                alert("You must select at least one character type!");
+                UC = confirm("Would you like to use uppercase letters?");
+                LC = confirm("Would you like to use lowercase letters?");
+                num = confirm("Would you like to use numbers?");
+                special = confirm("Would you like to use special characters?");
+        }
+        //calls generatePassword function
+                generatePassword();
+
+        //takes confirmed properties and puts them into the password variable
+        function generatePassword() {
+                var password = "";
+
+                var allowed = {};
+                if (UC) password += rando(allowed.UC = "QWERTYUIOPASDFGHJKLZXCVBNM"); //pulls in uppercase
+                if (LC) password += rando(allowed.LC = "qwertyuiopasdfghjklzxcvbnm"); //pulls in lowercase
+                if (num) password += rando(allowed.num = "1234567890"); //pulls in numbers
+                if (special) password += rando(allowed.symbols = "!@#$%^&*(){}[]=<>/,."); //pulls in special characters
+
+                //runs and adds a character each time until it reaches the length of the password
+                for (var i = password.length; i < length; i++) password += rando(rando(allowed).value);
+
+                document.getElementById("password").value = randoSequence(password).join("");
+        }
+
+        // Assignment Code
+        var generateBtn = document.querySelector("#generate");
+
+        // Writes password to the #password input
+        function writePassword() {
+                var password = generate.generatePassword();
+                var passwordText = document.querySelector("#password");
+
+                passwordText.value = password;
+
+        }
+
+        // Add event listener to generate button
+        generateBtn.addEventListener("click", writePassword);
+
 }
-
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-//onclick - present series of prompts
-        //prompt 1 select length of password 8-128 characters
-        //prompt 2 select if lowercase is allowed
-
-                //input validated and at least 1 character shown
-
-        //prompt 3 select if Uppercase is allowed
-                  //input validated and at least 1 character shown
-
-        //prompt 4 select if numeric is allowed
-                  //input validated and at least 1 character shown
-
-
-        //prompt 5 select if special characters are allowed
-                //input validated and at least 1 character shown
-
-        //after ALL prompts are answered, password is generated
-
-        //the password is then shown in an alert or written on the screen
-
